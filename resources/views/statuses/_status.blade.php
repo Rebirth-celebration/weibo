0f3b8730c56dbc6566326ea45d77fc3803bbca4b
Rebirth-celebration <786742044@qq.com>
2020-10-12 08:47:15 +0000

    删除用户

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,6 +1,6 @@
 <li class="media mt-4 mb-4">
     <a href="{{ route('users.show',$user->id) }}">
-        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar">
+        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
     </a>
     <div class="media-body">
         <h5 class="mt-0 mb-1">
@@ -9,4 +9,13 @@
         </h5>
         {{ $status->content }}
     </div>
+
+    @can('destroy',$status)
+        <form action="{{ route('statuses.destroy',$status->id) }}" method="POST"
+        onsubmit="return confirm('您确定要删除本条微博吗？  ')">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-sm btn-danger">删除</button>
+        </form>
+    @endcan
 </li>
