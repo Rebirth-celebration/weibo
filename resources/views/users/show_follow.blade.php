f54c9f10b7e1518af6adbb3060d4145680fd34fa
Rebirth-celebration <786742044@qq.com>
2020-10-13 06:39:04 +0000

    关注按钮

--- resources/views/users/show_follow.blade.php
+++ resources/views/users/show_follow.blade.php
@@ -8,8 +8,8 @@
         <div class="list-group list-group-flush">
             @foreach($users as $user)
                 <div class="list-group-item">
-                    <img class="mr-3" src="{{ $user->gravatar() }}" alt="$user->name" width=32>
-                    <a href="{{ route('user.show',$user) }}">
+                    <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32 />
+                    <a href="{{ route('users.show',$user) }}">
                         {{ $user->name }}
                     </a>
                 </div>
